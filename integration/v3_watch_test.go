788f26348f7b47190796e38c4883b1e771c398cb
Wojciech Cierpucha <wojciech.cierpucha@ntti3.io>
2020-08-24 14:24:33 +0200

    Add notice information to modified files required by license.

--- integration/v3_watch_test.go
+++ integration/v3_watch_test.go
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// NOTICE: As required by LICENSE's 4.(b), this file was changed by @cierpuchaw
+
 package integration
 
 import (
